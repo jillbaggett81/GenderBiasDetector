ff8963ea4db762a6200439416b7756bdd2a0db89
Jill Baggett <jbaggett1@gmail.com>
2020-11-06 16:57:21 -0600

    Updated JQuery and got form working:

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+import os
+from flask import Flask, request
 from flask.templating import render_template
+import logging
 
 app = Flask(__name__, template_folder='templates')
+print("logh")
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def hello():
@@ -10,6 +14,11 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hey {}!".format(name)
+
+@app.route('/process_letter_text', methods=["GET","POST"])
+def processLetter():
+    text = request.form.get("letterText")
+    return text + " this has been modified"
 
 if __name__ == '__main__':
     app.run(debug=True)
