6d613796ae91b6cc30d82941b46f8d63361b835d
Jill Baggett <jbaggett1@gmail.com>
2021-02-23 21:18:44 -0600

    Database

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from modules.process_text import ProcessText
 from modules.anonymizeText import anonText
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_restful import Api, Resource
 from flask.json import jsonify
 import json
 
@@ -21,8 +22,61 @@ app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # new
 db = SQLAlchemy(app)
 ma = Marshmallow(app) 
+api = Api(app)
 logging.basicConfig(level=logging.DEBUG)
 
+class Rec(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    #1 if male, 0 if female
+    gender = db.Column(db.Integer)
+    content = db.Column(db.String(500))
+
+    def __repr__(self):
+        return '<Review %s>' % self.content
+class RecSchema(ma.Schema):
+    class Meta:
+        fields = ("id","gender","content")
+        model = Rec
+#Create Schema for our DB model
+rec_schema = RecSchema()
+recs_schema = RecSchema(many=True)
+
+class PostListResource(Resource):
+    def get(self):
+        posts = Rec.query.all()
+        return recs_schema.dump(posts)
+    def post(self):
+        new_post = Rec(
+            gender=request.json['gender'],
+            content=request.json['content']
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return rec_schema.dump(new_post)
+    def patch(self, post_id):
+        post = Rec.query.get_or_404(post_id)
+
+        if 'gender' in request.json:
+            post.title = request.json['gender']
+        if 'content' in request.json:
+            post.content = request.json['content']
+
+        db.session.commit()
+        return rec_schema.dump(post)
+
+    def delete(self, post_id):
+        post = Rec.query.get_or_404(post_id)
+        db.session.delete(post)
+        db.session.commit()
+        return '', 204
+
+class PostResource(Resource):
+    def get(self, post_id):
+        post = Rec.query.get_or_404(post_id)
+        return rec_schema.dump(post)
+
+api.add_resource(PostResource, '/posts/<int:post_id>')
+api.add_resource(PostListResource, '/posts')
 #Home page for Flask
 #Run this application using "python3 app.py" then go to localhost:5000 on Chrome
 @app.route('/')
@@ -46,25 +100,18 @@ def processLetter():
     print(result)
     #Return results of the processing module in a new page
     current_app.logger.info(result.json['communal'])
+
+    #Post this to the DB
+
+    to_post = Rec( gender = 0, content = text)
+    db.session.add(to_post)
+    db.session.commit()
     return render_template('text_analysis.html', results = result.json)
 
 #We can eventually have this represent the results of machine learning for different words.
 #This also could represent different ML models
 #Src: https://rahmanfadhil.com/flask-rest-api/ 
-class Word(db.Model):
-    id = db.Column(db.String(100), primary_key=True)
-    weight = db.Column(db.Integer)
-    content = db.Column(db.Integer)
 
-    def __repr__(self):
-        return '<Word %s>' % self.id
-class WordSchema(ma.Schema):
-    class Meta:
-        fields = ("id", "weight", "content")
-        model = Word
-#Create Schema for our DB model
-word_schema = WordSchema()
-words_schema = WordSchema(many=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
